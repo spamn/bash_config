cce52675b45ce22f04d7baba39379799d22dc84e
spam <spam@via.ecp.fr>
2017-09-19 10:05:44 +0200

    Use gpg-agent for ssh

--- my_env.sh
+++ my_env.sh
@@ -5,4 +5,9 @@ export PATH=~/bin:$PATH
 #if not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-which vim >> /dev/null 2>&1 && export EDITOR=vim
+#Use gpg-agent for ssh
+export GPG_TTY=$(tty)
+which gpg-connect-agent > /dev/null 2>&1 && gpg-connect-agent updatestartuptty /bye >/dev/null
+[ -e "/run/user/$UID/gnupg/S.gpg-agent.ssh" ] && export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+
+which vim > /dev/null 2>&1 && export EDITOR=vim
