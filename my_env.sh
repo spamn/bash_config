8b99e456f11decfb56363d2d7a929a2aaeb70f2c
spam <spam@via.ecp.fr>
2017-09-21 21:23:29 +0200

    Add AWESOME_THEMES_PATH env variable.

--- my_env.sh
+++ my_env.sh
@@ -11,3 +11,7 @@ which gpg-connect-agent > /dev/null 2>&1 && gpg-connect-agent updatestartuptty /
 [ -e "/run/user/$UID/gnupg/S.gpg-agent.ssh" ] && export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
 
 which vim > /dev/null 2>&1 && export EDITOR=vim
+
+_AWESOME_THEMES_PATH="$HOME/.config/awesome/themes"
+[ -d "${_AWESOME_THEMES_PATH}" ] && export AWESOME_THEMES_PATH="${_AWESOME_THEMES_PATH}"
+unset _AWESOME_THEMES_PATH
