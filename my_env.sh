a3d3a61fe4d132d4d5b62bd30d0e8bbc3a465a8e
spamn <github@fleuryn.fr>
2019-04-30 08:52:32 +0200

    Don't enable gpg agent for root user.

--- my_env.sh
+++ my_env.sh
@@ -6,9 +6,11 @@ export PATH=~/bin:$PATH
 [[ $- != *i* ]] && return
 
 #Use gpg-agent for ssh
-export GPG_TTY=$(tty)
-which gpg-connect-agent > /dev/null 2>&1 && gpg-connect-agent updatestartuptty /bye >/dev/null
-[ -e "/run/user/$UID/gnupg/S.gpg-agent.ssh" ] && export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+if [ "$(id -u)" != "0" ] ; then
+  export GPG_TTY=$(tty)
+  which gpg-connect-agent > /dev/null 2>&1 && gpg-connect-agent updatestartuptty /bye >/dev/null
+  [ -e "/run/user/$UID/gnupg/S.gpg-agent.ssh" ] && export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+fi
 
 which vim > /dev/null 2>&1 && export EDITOR=vim
 
